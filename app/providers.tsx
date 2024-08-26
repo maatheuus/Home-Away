62cf38b4d2740f4c53598287eb1f7094ab6115f2
MATHEUS <matts14smkd@gmail.com>
2024-08-26 19:23:42 -0300

    create the providers for handle with the themes and the toaster component

---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "./theme-provider";
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Toaster />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default Providers;
