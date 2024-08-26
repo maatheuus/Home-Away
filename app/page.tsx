c0daa9d4c3db0c1a15173cfa641123bb376d0b93
MATHEUS <matts14smkd@gmail.com>
2024-08-26 19:23:00 -0300

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="default" className="m-8 capitalize">
+        click me
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
